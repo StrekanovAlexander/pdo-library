083a0a27a4c984bf95f4dd31635a3b6c6d5e59c7
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-23 11:21:12 +0300

    rebuilded: index

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 <?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$pdo = new \App\PDOLib('mysql:host=localhost;dbname=pdo_library_db', 'root', 'root');
+
+$pdo->createTable('users', ['id' => 'int', 'name' => 'varchar(255)']);
+// $pdo->dropTable('users');
