fac70361e2a8dbcc0377409a654e2973077ac5ae
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-24 10:52:36 +0300

    rebuilded: index

--- index.php
+++ index.php
@@ -3,46 +3,11 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 $pdo = new \PDO('mysql:host=localhost;dbname=pdo_library_db', 'root', 'root');
-// $pdo = new \App\PDOLib('mysql:host=localhost;dbname=pdo_library_db', 'root', 'root');
 
-// $pdo->createTable('users', [
-//   'id' => 'int', 
-//   'name' => 'varchar(255)',
-//   'from_' => 'varchar(255)',
-//   'age' => 'int',
-//   ]);
-// $pdo->dropTable('users');
+$query = new \App\PDOQuery($pdo, 'users');
 
-// $id = 4;
-// $name = 'Robert Mors';
-// $from = 'linkedin';
-// $age = '21';
+$query = $query->where('social', 'yahoo');
+$query = $query->select('id', 'name');
 
-// $id = 3;
-// $name = 'Paul Smith';
-// $name = "Forgery name2'); delete from users; --";
-
-// $sql = "insert into users values({$id}, '{$name}', '{$from}', {$age})";
-// $pdo->exec($sql);
-
-// print_r($sql);
-// $pdo->exec("insert into users values({$id}, '{$name}')");
-
-// $data = $pdo->query("select * from users")->fetchAll();
-// print_r($data);
-
-
-$query = new \App\PDOQuery($pdo, 'users', []);
-$query = $query->where('id', 2);
-$query = $query->where('from_', 'github');
-$query = $query->where('age', 23);
-// $sql = $query->toSql();
-// print_r($sql);
-// $data = $pdo->query($sql)->fetchAll();
-// print_r($data);
-// $query->toSql();
-// print_r($query->toSql());
-
-
-$data = $query->all();
-var_dump($data);
+var_dump(sizeof($query->all()));
+var_dump($query->count());
