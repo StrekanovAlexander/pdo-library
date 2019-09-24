60eccd51c6937805806bf64cb390ccb8bf1b8659
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-24 14:15:00 +0300

    rebuilded: index

--- index.php
+++ index.php
@@ -6,8 +6,13 @@ $pdo = new \PDO('mysql:host=localhost;dbname=pdo_library_db', 'root', 'root');
 
 $query = new \App\PDOQuery($pdo, 'users');
 
-$query = $query->where('social', 'yahoo');
+$query = $query->where('social', 'github');
 $query = $query->select('id', 'name');
 
-var_dump(sizeof($query->all()));
-var_dump($query->count());
+// var_dump(sizeof($query->all()));
+// var_dump($query->count());
+
+$coll = $query->map(function($row) {
+  return $row['id'] . '-' . $row['name'];
+});
+var_dump($coll);
