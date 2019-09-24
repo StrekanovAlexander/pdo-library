80651acd4ea3260fa26ff8d6f24c25549059db09
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-24 10:52:25 +0300

    rebuilded: PDOQuery class

--- classes/PDOQuery.php
+++ classes/PDOQuery.php
@@ -5,42 +5,69 @@ class PDOQuery
 {
   private $pdo;
   private $table;
-  private $where;
+  private $data = [
+    'select' => '*',
+    'where' => [],
+  ];
 
-  public function __construct($pdo, $table, $where = [])
+  public function __construct($pdo, $table, $data = null)
   {
     $this->pdo = $pdo;
     $this->table = $table;
-    $this->where = $where;
+    if ($data) {
+      $this->data = $data;
+    }
   }
 
-  private function getClone($where) {
-    $mergedData = array_merge($this->where, $where);
+  public function where($key, $value)
+  {
+    $data = ['where' => array_merge($this->data['where'], [$key => $value])];
+    return $this->getClone($data);
+  }
+
+  private function getClone($data)
+  {
+    $mergedData = array_merge($this->data, $data);
     return new self($this->pdo, $this->table, $mergedData);
   }
 
-  public function where($key, $value) {
-    $where = [$key => $value];
-    return $this->getClone($where);
+  public function select(...$arguments)
+  {
+    $select = implode(', ', $arguments);
+    return $this->getClone(['select' => $select]);
   }
 
-  public function toSql() {
-
-    $sql = "select * from {$this->table}";
-    if (!empty($this->where)) {
-      $data = [];
-      foreach ($this->where as $key => $value) {
-        $data[] = "{$key} = {$this->pdo->quote($value)}"; 
-      }
-      $data = join(' AND ', $data);
-      $sql .= " where " . $data;
-    } 
-    return $sql;
+  private function buildWhere()
+  {
+    return implode(' AND ', array_map(function ($key, $value) {
+      $quotedValue = $this->pdo->quote($value);
+        return "$key = $quotedValue";
+      }, array_keys($this->data['where']), $this->data['where']));
   }
+
+  public function toSql()
+  {
+    $sqlParts = [];
+    $sqlParts[] = "SELECT {$this->data['select']} FROM {$this->table}";
+    if ($this->data['where']) {
+      $where = $this->buildWhere();
+      $sqlParts[] = "WHERE $where";
+    }
+    return implode(' ', $sqlParts);
+  }  
 
   public function all()
   {
     return $this->pdo->query($this->toSql())->fetchAll();
   }
 
+  public function count()
+  {
+    $sql = "select count(*) from $this->table";
+    if ($this->data['where']) {
+      $where = $this->buildWhere();
+      $sql .= " WHERE $where";
+    }
+    return $this->pdo->query($sql)->fetchColumn();
+  }
 } 
