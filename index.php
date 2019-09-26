c54e38d130d64d0476d0cddeec4e1a6b12d67355
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-26 21:26:21 +0300

    rebuild: index

--- index.php
+++ index.php
@@ -3,14 +3,37 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $pdo = \App\PDO::pdo();
 
-$mapper = new \App\UserMapper($pdo);
+function like($pdo, array $params) {
+  $sql = 'SELECT id FROM users';
+  if (!$params) {
+    $stmt = $pdo->query($sql); 
+    $stmt->execute(); 
+  } else {
+    $fields = array_map(function ($field) {
+      return "{$field} LIKE ?";
+    }, array_keys($params));
+    $sql .= ' WHERE ' . join(' OR ', $fields);
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array_values($params));
+  }
+  return $stmt->fetchAll(PDO::FETCH_COLUMN);
+  
+}
 
-$user = new \App\User('Paul Smith');
-$user->addPhoto('family1', '/path/to/photo/family1');
-$user->addPhoto('party1', '/path/to/photo/party1');
-$user->addPhoto('friends1', '/path/to/photo/friends1');
+$params = ['name' => '%Paul%', 'social' => 'ya%'];
 
-$mapper = new \App\UserMapper($pdo);
-$mapper->save($user);
+$result = like($pdo, $params);
+// $result = like($pdo, []);
 
-echo 'Ok';
+print_r($result);
+
+// if (empty($params)) {
+  //   $stmt = $pdo->query('SELECT id FROM users'); 
+  //   $stmt->execute(); 
+  // } else {
+    // $name1 = '%Paul%';
+    // $name2 = 'ya%';
+    // $stmt = $pdo->prepare('SELECT id FROM users WHERE name LIKE ? OR social LIKE ?');
+    // $stmt->execute([$name1, $name2]);
+    //return $stmt->fetchAll(PDO::FETCH_COLUMN);
+  // }  
