912c5fd4a99061b965b99b834c0a7288af74af17
StrekanovAlexander <strekanov.alexander@gmail.com>
2019-09-27 12:26:20 +0300

    rebuilded: index

--- index.php
+++ index.php
@@ -38,16 +38,28 @@ $pdo = \App\PDO::pdo();
     //return $stmt->fetchAll(PDO::FETCH_COLUMN);
   // }  
 
-echo "In operator";
-echo "<br>";
-
-$idx = [1, 3, 4];
-
-$in = implode(", ", array_fill(0, sizeof($idx), '?'));
-$sql = "SELECT name FROM users WHERE id IN ($in)";
-
-$stmt = $pdo->prepare($sql);
-$stmt->execute($idx);
-
-print_r($stmt->fetchAll(\PDO::FETCH_ASSOC));
+// echo "In operator";
+// echo "<br>";
+// $idx = [1, 3, 4];
+// $in = implode(", ", array_fill(0, sizeof($idx), '?'));
+// $sql = "SELECT name FROM users WHERE id IN ($in)";
+// $stmt = $pdo->prepare($sql);
+// $stmt->execute($idx);
+// print_r($stmt->fetchAll(\PDO::FETCH_ASSOC));
   
+
+echo "where with sort <br>";
+
+function where($pdo, array $params = []) {
+  $sql = "SELECT id FROM users";
+  $where = [];
+  foreach ($params as $key => $value) {
+    $where[] = "{$key} IN ({$value})";
+  }
+  $where = " WHERE " . implode(" OR ", $where);
+  
+  return $sql . $where;
+}
+
+$result = where($pdo, ['name' => 'Paul Smith', 'social' => 'github']);
+print_r($result);
